62a52d82cafb68327b84d030a94a3e72eaea22f1
Radu Pop <popraducj@yahoo.com>
2016-11-30 01:48:03 +0200

    initial commit

---
+++ RockPaperScissors/RockPaperScissors.WebApi.Tests/Controllers/RockPaperScissorsGeneratorControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RockPaperScissors.WebApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RockPaperScissors.StaticValues;
+
+namespace RockPaperScissors.WebApi.Tests.Controllers
+{
+    [TestClass]
+    public class RockPaperScissorsGeneratorControllerTest
+    {
+        private RockPaperScissorsGeneratorController _controller ;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _controller = new RockPaperScissorsGeneratorController();
+        }
+
+        [TestMethod]
+        public void GetRandomPreference()
+        {
+            var choise = _controller.GetRandomPreference();
+            Assert.IsTrue(Enum.IsDefined(typeof(Possibilities), choise), "The result is not for rock paper scissors game");
+
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _controller.Dispose();
+        }
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors.WebApi/Controllers/RockPaperScissorsGeneratorController.cs
@@ -0,0 +1,18 @@
+﻿
+
+using RockPaperScissors.StaticValues;
+using System;
+using System.Web.Http;
+namespace RockPaperScissors.WebApi.Controllers
+{
+    public class RockPaperScissorsGeneratorController : ApiController
+    {
+        //
+        // GET: /RockPaperScissorsGenerator/
+        public Possibilities GetRandomPreference()
+        {
+            var randomPosibility = new Random().Next(3);
+            return (Possibilities)randomPosibility;
+        }
+	}
+}
